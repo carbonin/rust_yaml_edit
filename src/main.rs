ab2000da540b4345dd40d26443d3c2aae1a2c0c8
Nick Carboni <ncarboni@redhat.com>
2023-08-29 11:56:37 -0400

    working first pass

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use serde_json::Value;
+use serde_json::{Value, json};
 
 fn main() {
     let data = r#"
@@ -11,7 +11,52 @@ fn main() {
         },
         "spec":{"foo": "bar"}
     }"#;
-    let v: Value = serde_json::from_str(data).unwrap();
-
+    let mut v: Value = serde_json::from_str(data).unwrap();
+    add_annotation(&mut v);
     println!("annotations: {}", v["metadata"]["annotations"]);
+
+    let data = r#"
+    {
+        "metadata": {
+            "name": "thing"
+        },
+        "spec":{"foo": "bar"}
+    }"#;
+    let mut v: Value = serde_json::from_str(data).unwrap();
+    add_annotation(&mut v);
+    println!("annotations: {}", v["metadata"]["annotations"]);
+
+    let data = r#"
+    {
+        "spec":{"foo": "bar"}
+    }"#;
+    let mut v: Value = serde_json::from_str(data).unwrap();
+    add_annotation(&mut v);
+    println!("annotations: {}", v["metadata"]["annotations"]);
+}
+
+fn add_annotation(
+    resource: &mut Value
+) {
+    if let Some(annotations) = resource.pointer_mut("/metadata/annotations") {
+        // annotations exist, add new key
+        if let Some(obj) = annotations.as_object_mut() {
+            obj.insert(String::from("recert-edited"), json!("/some/json/path/here"));
+        }
+    } else {
+        // annotations don't exist add new map at annotations key in metadata
+        let mut a = serde_json::Map::new();
+        a.insert(String::from("recert-edited"), json!("/some/json/path/here"));
+        if let Some(metadata) = resource.pointer_mut("/metadata") {
+            if let Some(obj) = metadata.as_object_mut() {
+                obj.insert(String::from("annotations"), Value::Object(a));
+            }
+        } else {
+            let mut metadata = serde_json::Map::new();
+            metadata.insert(String::from("annotations"), Value::Object(a));
+            if let Some(obj) = resource.as_object_mut() {
+                obj.insert(String::from("metadata"), Value::Object(metadata));
+            }
+        }
+    }
 }
